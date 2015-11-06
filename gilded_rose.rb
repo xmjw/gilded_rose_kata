dcdd11eb2ef0527b92468f16201b38b5bdfea02d
Duncan Stuart <dgmstuart@gmail.com>
2015-11-06 11:33:57 +0000

    refactoring to max_quality? and removing WOW item as it does not decay

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,50 +6,52 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
+      if item.name == 'Sulfuras, Hand of Ragnaros'
+        next
+      end
+
       if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality -= 1
-          end
+          item.quality -= 1
         end
       else
-        if item.quality < 50
+        if !max_quality?(item)
           item.quality += 1
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
+              item.quality += 1
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
+              item.quality += 1
             end
           end
         end
       end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in -= 1
-      end
+
+      item.sell_in -= 1
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
             if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality -= 1
-              end
+              item.quality -= 1
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-          if item.quality < 50
+          if !max_quality?(item)
             item.quality += 1
           end
         end
       end
     end
+  end
+
+  private
+
+  def max_quality?(item)
+    item.quality >= 50
   end
 
 end
